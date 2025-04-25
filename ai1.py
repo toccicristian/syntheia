69398d5ffdd442870445dbba92e329f88a3eb686
Cristian Tocci <cristian@pcofi.pcofi.org>
2025-04-25 01:12:03 -0300

    cambiar estrategia

---
+++ ai1.py
@@ -0,0 +1,100 @@
+import sys
+from os.path import normpath, expanduser
+
+
+############################################################
+# BIBLIOTECA DE RECUERDOS
+#
+import modelos.recuerdo_modelo as recmodel
+import repositorios.recuerdos as recrepo
+#
+if not recrepo.busca_recuerdo_por_nombre("nombreUsuario"):
+    recuerdo_apodo=recmodel.Recuerdo(nombre="nombreUsuario", contenido=[input("Quien sos?\n>"),])
+    recrepo.agrega_recuerdo(recuerdo_apodo)
+#
+#
+recuerdo_apodo=recrepo.busca_recuerdo_por_nombre("nombreUsuario")
+apodo = recuerdo_apodo.contenido[0]
+#
+#
+if not recrepo.busca_recuerdo_por_nombre("quiensoy"):
+    recuerdo_botname=recmodel.Recuerdo(nombre="quiensoy", contenido=[input(f"No recuerdo mi nombre... cuál es?\n{apodo}>"),])
+    recrepo.agrega_recuerdo(recuerdo_botname)
+#
+recuerdo_botname=recrepo.busca_recuerdo_por_nombre("quiensoy")
+botname = recuerdo_botname.contenido[0]
+#
+############################################################
+
+
+
+############################################################
+#                  DATASET:
+#
+#INTENTS_FILE="datasets/university_dataset.json"
+INTENTS_FILE="datasets/Conversation.csv"
+#
+#
+#
+############################################################
+
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+from chatterbot.trainers import ChatterBotCorpusTrainer
+
+chatbot = ChatBot(botname, storage_adapter='chatterbot.storage.SQLStorageAdapter') # Crear un nuevo chatbot
+trainer = ListTrainer(chatbot)  # Crear un entrenador
+
+#ENTRENAMOS EL CHATBOT SEGÚN SEA JSON O CSV EL INTENTS:
+
+if INTENTS_FILE.lower().endswith(".json"):
+    with open(INTENTS_FILE, 'r', encoding='utf-8') as file:
+        import json
+        intents = json.load(file)
+        for intent in intents['intents']:
+            trainer.train(intent['patterns'])
+            trainer.train(intent['responses'])
+
+else:
+    if INTENTS_FILE.lower().endswith(".csv"):
+        import pandas as pd
+
+        if os.path.exists(INTENTS_FILE):
+            data = pd.read_csv(INTENTS_FILE)
+        else:
+            data = pd.DataFrame(columns=['question', 'answer'])
+
+    i=1
+    for index, row in data.iterrows():
+        question = row['question']
+        answer = row['answer']
+        print(f"[{i}/{len(data)}]",end="",flush=True)
+        trainer.train([question, answer]) # Entrenar con la pregunta y la respuesta
+        i=i+1
+
+
+
+try:
+    while True:
+        user_input = input(f"{apodo}>")
+        if user_input.lower() in ["salir", "exit"]:
+            print("Bot: ¡Hasta luego!")
+            break
+        response = chatbot.get_response(user_input)
+        if response.confidence < 0.6:
+            print("Bot: Sorry, i didn't get that. Could you rephrase?")
+            if INTENTS_FILE.lower().endswith(".csv"):
+                user_feedback = input("Would you like me to learn the answer to this question? (yes/no): ")
+                if user_feedback.lower() == 'yes':
+                    new_answer = input("which would be the right answer? ")
+                    new_data = pd.DataFrame([[user_input, new_answer]], columns=['question', 'answer'])
+                    data = pd.concat([data, new_data], ignore_index=True)
+                    data.to_csv(INTENTS_FILE, index=False)
+                    trainer.train([user_input, new_answer])
+        else:
+            print(f"{botname}>{response}")
+
+except KeyboardInterrupt:
+    print("\nPrograma finalizado!")
+    sys.exit()
+
