61471fe6a018deeb688c45cfaef2d6cecc3e07d4
Cristian Tocci <cristian@pcofi.pcofi.org>
2025-05-02 18:12:35 -0300

    agregar scripts de trabajo

---
+++ ai_post_push.sh
@@ -0,0 +1,9 @@
+mv pre-push/datasets/* datasets/
+mv pre-push/db/* ./
+mv pre-push/recuerdos/* ./
+
+rmdir pre-push/datasets pre-push/db pre-push/recuerdos
+rmdir pre-push
+
+
+
---
+++ ai_pre_push.sh
@@ -0,0 +1,11 @@
+mkdir -p pre-push/datasets
+mkdir -p pre-push/db
+mkdir -p pre-push/recuerdos
+
+mv datasets/*csv* datasets/*CSV* datasets/*json* datasets/*JSON* pre-push/datasets/
+mv db* pre-push/db/
+mv recuerdos.json pre-push/recuerdos/
+
+
+
+
